914826895ce9034edd6254b92d13c8336ecca71f
Astral <bakadao@mail.com>
2025-05-08 14:28:10 +0900

    Create signaling.js

---
+++ js/signaling.js
@@ -0,0 +1,28 @@
+let socket;
+
+function connectAchex(userId) {
+  // config.jsで設定したグローバル変数を使用
+  socket = new WebSocket(window.ACHEX_URL);  // ここで設定を使用
+  socket.addEventListener('open', () => {
+    socket.send(JSON.stringify({ auth: { user: userId, pass: '' } }));
+    socket.send(JSON.stringify({ room: window.ROOM_ID, data: { type: 'join', user: userId } }));
+  });
+
+  socket.addEventListener('message', async (ev) => {
+    const msg = JSON.parse(ev.data);
+    if (msg.data?.type === 'join' && msg.from !== userId) {
+      createPeerConnection(msg.from, true);
+    }
+    if (msg.data?.type === 'signal') {
+      await handleSignal(msg.from, msg.data.signal);
+    }
+  });
+}
+
+function sendSignal(to, signal) {
+  socket.send(JSON.stringify({ room: window.ROOM_ID, to, data: { type: 'signal', signal } }));
+}
+
+// connectAchex と sendSignal をグローバルに公開
+window.connectAchex = connectAchex;
+window.sendSignal = sendSignal;
