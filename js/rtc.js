a1bbb4536dbb0ebf88850e5a392b6bf0ab942949
Astral <bakadao@mail.com>
2025-05-09 14:39:50 +0900

    Update rtc.js

--- js/rtc.js
+++ js/rtc.js
@@ -1,42 +1,50 @@
 const peers = {};
 
+// 新しい PeerConnection を作成 or 既存を再利用
 async function createPeerConnection(peerId, isInitiator) {
   if (peers[peerId]) return;
-  const pc = new RTCPeerConnection({ iceServers:[{urls:'stun:stun.l.google.com:19302'}] });
+  const pc = new RTCPeerConnection({
+    iceServers:[{ urls:'stun:stun.l.google.com:19302' }]
+  });
   peers[peerId] = pc;
 
-  pc.onicecandidate = e => e.candidate && sendSignal(peerId, { candidate:e.candidate });
-  let dc;
+  pc.onicecandidate = e => {
+    if (e.candidate) {
+      sendSignal(peerId, { candidate: e.candidate });
+    }
+  };
+
   if (isInitiator) {
-    dc = pc.createDataChannel('chat');
+    const dc = pc.createDataChannel('chat');
     setupDataChannel(dc);
-    peers[peerId].dataChannel = dc;
+    pc.dataChannel = dc;
     const offer = await pc.createOffer();
     await pc.setLocalDescription(offer);
-    sendSignal(peerId, { sdp:pc.localDescription });
+    sendSignal(peerId, { sdp: pc.localDescription });
   } else {
     pc.ondatachannel = e => {
       setupDataChannel(e.channel);
-      peers[peerId].dataChannel = e.channel;
+      pc.dataChannel = e.channel;
     };
   }
 
-  async function handleSignal(from, signal) {
+  // シグナルの受信処理をグローバル化
+  window.handleSignal = async (from, signal) => {
+    const conn = peers[from];
     if (signal.sdp) {
-      await pc.setRemoteDescription(signal.sdp);
+      await conn.setRemoteDescription(signal.sdp);
       if (signal.sdp.type === 'offer') {
-        const ans = await pc.createAnswer();
-        await pc.setLocalDescription(ans);
-        sendSignal(from, { sdp:pc.localDescription });
+        const ans = await conn.createAnswer();
+        await conn.setLocalDescription(ans);
+        sendSignal(from, { sdp: conn.localDescription });
       }
     } else if (signal.candidate) {
-      await pc.addIceCandidate(signal.candidate);
+      await conn.addIceCandidate(signal.candidate);
     }
-  }
-
-  window.handleSignal = handleSignal;
+  };
 }
 
+// DataChannel の共通セットアップ
 function setupDataChannel(channel) {
   channel.onopen = () => console.log('DataChannel open');
   channel.onmessage = e => {
